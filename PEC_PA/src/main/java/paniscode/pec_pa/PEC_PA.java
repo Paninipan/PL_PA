fead5ccff936608eee65b639158dcf2f7286da63
ÁlvaroPC <alvaropc2362005@gmail.com>
2025-04-22 16:45:52 +0200

    Inicializacion del repositorio?

---
+++ PEC_PA/src/main/java/paniscode/pec_pa/PEC_PA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paniscode.pec_pa;
+
+/**
+ *
+ * @author alvaro
+ */
+public class PEC_PA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
