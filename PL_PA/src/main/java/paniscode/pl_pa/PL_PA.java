f1b03bb1dad7e897d4fd9a1cc4d0c0db91b0bcaa
ÁlvaroPC <alvaropc2362005@gmail.com>
2025-04-22 16:57:57 +0200

    Inicializacion del repositorio?

---
+++ PL_PA/src/main/java/paniscode/pl_pa/PL_PA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paniscode.pl_pa;
+
+/**
+ *
+ * @author alvaro
+ */
+public class PL_PA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
